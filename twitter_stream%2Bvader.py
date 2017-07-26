f24b39811185ee50f8e9f0a3d56fad366deb17f4
Francis Cowper <franciscowper@gmail.com>
2017-07-26 09:41:00 +0100

    Added a try except clause to deal with exceptions

--- twitter_stream%2Bvader.py
+++ twitter_stream%2Bvader.py
@@ -21,31 +21,38 @@ from pandas import DataFrame, Series
 conn = pyodbc.connect(r'DSN=twitter_stream_netflix')
 cur = conn.cursor()
 
-
-class StdOutListener(StreamListener):
-    def on_data(self, data):  # pulling the text out of the JSON file each tweet comes in
-        json_load = json.loads(data)
-        texts = json_load['text']
-        coded = texts.encode('utf-8')
-        s = str(coded)
-        ss = SentimentIntensityAnalyzer().polarity_scores(s)
-        datetime = json.loads(data)['created_at']  # print the datetime of the tweet
-        text = s[1:]  # print the text of the tweet
-        compound = ss.get('compound', '0')
-        negative = ss.get('neg', '0')
-        positive = ss.get('pos', '0')
-        neutral = ss.get('neu', '0')  # print the VADER sentiment scores for the tweet
-        user_id = json.loads(data)['user'].get('id', 'NA')
-        user_name = json.loads(data)['user'].get('name', 'NA')
-        user_screen_name = json.loads(data)['user'].get('screen_name', 'NA')
-        location = json.loads(data)['user'].get('location', 'NA')
-        num_of_followers = json.loads(data)['user'].get('followers_count', 'NA')
-        time_zone = json.loads(data)['user'].get('time_zone', 'NA')
-        cur.execute('insert into twitter_stream_netflix (datetime_created, user_id, user_name, user_screen_name,\
+try:
+    class StdOutListener(StreamListener):
+        def on_data(self, data):  # pulling the text out of the JSON file each tweet comes in
+            json_load = json.loads(data)
+            texts = json_load['text']
+            coded = texts.encode('utf-8')
+            s = str(coded)
+            ss = SentimentIntensityAnalyzer().polarity_scores(s)
+            datetime = json.loads(data)['created_at']  # print the datetime of the tweet
+            text = s[1:]  # print the text of the tweet
+            compound = ss.get('compound', '0')
+            negative = ss.get('neg', '0')
+            positive = ss.get('pos', '0')
+            neutral = ss.get('neu', '0')  # print the VADER sentiment scores for the tweet
+            user_id = json.loads(data)['user'].get('id', 'NA')
+            user_name = json.loads(data)['user'].get('name', 'NA')
+            user_screen_name = json.loads(data)['user'].get('screen_name', 'NA')
+            location = json.loads(data)['user'].get('location', 'NA')
+            num_of_followers = json.loads(data)['user'].get('followers_count', 'NA')
+            time_zone = json.loads(data)['user'].get('time_zone', 'NA')
+            cur.execute('insert into twitter_stream_netflix (datetime_created, user_id, user_name, user_screen_name,\
+             location, num_of_followers, time_zone, tweet_text, vader_compound, vader_negative,\
+            vader_positive, vader_neutral) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', datetime, user_id, user_name,\
+                        user_screen_name, location, num_of_followers, time_zone, text, compound, negative, positive, neutral)
+            conn.commit()  # insert values into SQL database
+except:
+    cur.execute('insert into twitter_stream_netflix (datetime_created, user_id, user_name, user_screen_name,\
          location, num_of_followers, time_zone, tweet_text, vader_compound, vader_negative,\
-        vader_positive, vader_neutral) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', datetime, user_id, user_name,\
-                    user_screen_name, location, num_of_followers, time_zone, text, compound, negative, positive, neutral)
-        conn.commit()  # insert values into SQL database
+        vader_positive, vader_neutral) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', 'NA', 'NA', 'NA', \
+                'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA')
+    conn.commit() # accounting for blank values
+    print ('YEEEEEEEEEEEEH BWOI SORTED MATE')
 
     def on_error(self, status):
         print(status)
